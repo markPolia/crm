47e054bbbca793b4ff01e3bf9bd86717251b61e6
Augenstern_ <2415286134@qq.com>
2022-11-14 20:12:10 +0800

    crm

---
+++ src/main/java/com/powernode/web/util/DateTimeUtil.java
@@ -0,0 +1,67 @@
+package com.powernode.web.util;
+
+import java.util.Date;
+
+public class DateTimeUtil {
+    private final static String DATE_FORMAT = "%1$tY-%1$tm-%1$td";
+    private final static String TIME_FORMAT = "%1$tY-%1$tm-%1$td %1$tT";
+
+    private DateTimeUtil() {
+    }
+
+    /**
+     * <pre>
+     *  返回当前时间的19位表示方式
+     *  example 2022-11-14 19:54:19</pre>
+     *   @return 返回当前时间
+     */
+    public static String generateNowTime() {
+        return String.format(DATE_FORMAT, new Date());
+    }
+
+    /**
+     * <pre>
+     *  返回当前日期的10位表示方式
+     *  example 2022-11-14</pre>
+     * @return 返回当前日期
+     */
+    public static String generateNowDate() {
+        return String.format(TIME_FORMAT, new Date());
+    }
+
+    /**
+     *  时间格式化
+     * @param time 要格式化的时间
+     * @return 时间的字符串表达形式
+     */
+    public static String formatTime(Date time) {
+        return String.format(TIME_FORMAT, time);
+    }
+
+    /**
+     *  时间格式化
+     * @param time 要格式化的时间
+     * @return 时间的字符串表达形式
+     */
+    public static String formatTime(java.sql.Date time) {
+        return String.format(TIME_FORMAT, time);
+    }
+
+    /**
+     *  日期格式化
+     * @param date 要格式化的日期
+     * @return 日期的字符串表达形式
+     */
+    public static String formatDate(Date date) {
+        return String.format(DATE_FORMAT, date);
+    }
+
+    /**
+     *  日期格式化
+     * @param date 要格式化的日期
+     * @return 日期的字符串表达形式
+     */
+    public static String formatDate(java.sql.Date date) {
+        return String.format(DATE_FORMAT, date);
+    }
+}
--- src/test/java/com/powermode/web/ConfigTest.java
+++ src/test/java/com/powermode/web/ConfigTest.java
@@ -1,13 +1,12 @@
 package com.powermode.web;
 
 import com.powernode.web.SpringConfig;
+import com.powernode.web.util.DateTimeUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.util.Date;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
@@ -21,10 +20,9 @@ public class ConfigTest {
 
     @Test
     public void expireDateTest() {
-        String date = "2022-11-13";
-        String now = String.format("%1$tY-%1$tm-%1$td", new Date());
-        System.out.println("now " + now);
-        System.out.println("date " + date);
-        System.out.println(date.compareTo(now));
+        String date = "2022-11-15";
+        System.out.println(date.compareTo(DateTimeUtil.generateNowDate()));
+
+
     }
 }
